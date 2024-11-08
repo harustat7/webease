497815681bd331f19fb92d091e6707e3e3721aac
Harustat Kaur <harustatkaur10@gmail.com>
2024-11-08 16:12:10 +0530

    create

---
+++ second_page(login)/script.js
@@ -0,0 +1,3 @@
+document.getElementsByName('create')[0].addEventListener('click',function(){
+    window.location.href="/third_page(create an account)/index.html";
+});
---
+++ third_page(create an account)/script.js
@@ -0,0 +1,3 @@
+document.getElementsByName('login')[0].addEventListener('click',function(){
+    window.location.href="/third_page(create an account)/index.html";
+});

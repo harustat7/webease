e4870971b5e4f04cf8c8f8fa7dfb951c56d7d626
Priyakaduar <priyakaduar2003@gmail.com>
2024-11-23 13:55:51 +0000

    webease

--- blogging/Blogge-1.0.0.0/Blogge-1.0.0.0/js/script.js
+++ blogging/Blogge-1.0.0.0/Blogge-1.0.0.0/js/script.js
@@ -1,1 +1,26 @@
 $(document).ready(function(){"use strict";document.getElementById("copyrightYear").innerHTML=(new Date).getFullYear(),$(".widget-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,slidesToScroll:1,arrows:!0,autoplay:!0,responsive:[{breakpoint:992,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]}),$(window).on("scroll",function(){$(window).scrollTop()?$("nav").addClass("nav-bg"):$("nav").removeClass("nav-bg")})});
+const templateContent = document.querySelector('.template').innerHTML;
+
+document.querySelectorAll('.editable_image').forEach((image) => {
+
+  image.addEventListener('click', function () {
+    const input = document.createElement('input'); 
+    input.type = 'file';
+    input.accept = 'image/*';
+
+    input.click();
+
+    input.addEventListener('change', (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+          image.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file); 
+      }
+});
+});
+});
